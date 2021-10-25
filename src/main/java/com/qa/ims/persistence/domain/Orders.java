05c1e53f853a88d61e751c080f8b00e04d1a092f
unknown <alzein7@outlook.com>
2021-10-25 14:46:41 +0100

    IMS-21 #comment this is my orders domain file

---
+++ src/main/java/com/qa/ims/persistence/domain/Orders.java
@@ -0,0 +1,98 @@
+package com.qa.ims.persistence.domain;
+
+import java.sql.Date;
+
+public class Orders {
+	
+	private Long orderId;
+	private Date orderDate;
+	private Long customerId;
+	private Long itemId;
+	
+	public Orders(Long orderId, Date orderDate, Long customerId, Long itemId) {
+		super();
+		this.orderId = orderId;
+		this.orderDate = orderDate;
+		this.customerId = customerId;
+		this.itemId = itemId;
+		
+	}
+	public Orders(Date orderDate, Long customerId, Long itemId) {
+		super();
+		this.orderDate = orderDate;
+		this.customerId = customerId;
+		this.itemId = itemId;
+	}
+	
+	
+	public Long getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public Long getItemId() {
+		return itemId;
+	}
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+	@Override
+	public String toString() {
+		return "Orders [orderId=" + orderId + ", orderDate=" + orderDate + ", customerId=" + customerId + ", itemId="
+				+ itemId + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((customerId == null) ? 0 : customerId.hashCode());
+		result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
+		result = prime * result + ((orderDate == null) ? 0 : orderDate.hashCode());
+		result = prime * result + ((orderId == null) ? 0 : orderId.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Orders other = (Orders) obj;
+		if (customerId == null) {
+			if (other.customerId != null)
+				return false;
+		} else if (!customerId.equals(other.customerId))
+			return false;
+		if (itemId == null) {
+			if (other.itemId != null)
+				return false;
+		} else if (!itemId.equals(other.itemId))
+			return false;
+		if (orderDate == null) {
+			if (other.orderDate != null)
+				return false;
+		} else if (!orderDate.equals(other.orderDate))
+			return false;
+		if (orderId == null) {
+			if (other.orderId != null)
+				return false;
+		} else if (!orderId.equals(other.orderId))
+			return false;
+		return true;
+	}
+}
